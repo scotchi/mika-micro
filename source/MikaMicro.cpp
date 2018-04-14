0c3579dfe678577d006e99e4bdce4e2a8ff43053
Andrew Minnich <aminnich3@gmail.com>
2018-04-13 22:34:46 -0400

    initial commit

---
+++ source/MikaMicro.cpp
@@ -0,0 +1,40 @@
+#include "MikaMicro.h"
+#include "IPlug_include_in_plug_src.h"
+#include "IControl.h"
+#include "resource.h"
+
+MikaMicro::MikaMicro(IPlugInstanceInfo instanceInfo)
+  :	IPLUG_CTOR(0, 128, instanceInfo)
+{
+	TRACE;
+
+	IGraphics* pGraphics = MakeGraphics(this, GUI_WIDTH, GUI_HEIGHT, 120);
+	pGraphics->AttachPanelBackground(&COLOR_GRAY);
+
+	IBitmap knob = pGraphics->LoadIBitmap(KNOB_ID, KNOB_FN, 60);
+
+	AttachGraphics(pGraphics);
+
+	MakeDefaultPreset("-", 128);
+}
+
+MikaMicro::~MikaMicro() {}
+
+void MikaMicro::ProcessDoubleReplacing(double** inputs, double** outputs, int nFrames)
+{
+	for (int s = 0; s < nFrames; s++)
+	{
+		outputs[0][s] = outputs[1][s] = 0.0;
+	}
+}
+
+void MikaMicro::Reset()
+{
+	TRACE;
+	IMutexLock lock(this);
+}
+
+void MikaMicro::OnParamChange(int paramIdx)
+{
+	IMutexLock lock(this);
+}
